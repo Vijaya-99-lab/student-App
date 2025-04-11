dd85d9b2134bdf05b8c6791ac971574480d6ece5
Vijaya-99-lab <Vijaya.shravan9@gmail.com>
2025-04-11 17:10:18 -0400

    Update Orders.js

--- src/Orders.js
+++ src/Orders.js
@@ -6,7 +6,9 @@ return (
   <div> New Car Orders </div> 
    <h1> Acura 100 orders  </h1>
    <h1> Honda 200 orders  </h1>
-   <h1> Ford 300 orders  </h1>
+   <h2> Ford 300 orders  </h2>
+ <h2> Benj 300 orders  </h2>
+ <h2> Posche 400 orders  </h2>
 
   </div>
   
